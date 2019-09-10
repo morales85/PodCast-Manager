f1958c5786d6b62f871e570ab2ca38dc53c42982
morales85 <mor4les85@gmail.com>
2019-09-10 11:53:44 +0300

    pod

---
+++ dist/main.js
@@ -0,0 +1,105 @@
+const renderer = new Renderer()
+const podManager = new PodManager()
+const srtGenres = new SortedGenres()
+const createAudio = new audioPlay()
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+})
+
+let loadPage = function () {
+    renderer.renderGenres(srtGenres.genres)
+}
+loadPage()
+
+const getPods = async function () {
+    let query = $("#podcastInput").val()
+    $("#podcastInput").val('')
+    await podManager.getPodData(query)
+    console.log(query)
+    renderer.render(podManager.podData)
+}
+
+$('.pods').on('click','.img-genre', async function () {
+    let genreId = $(this).data('id')
+    let data = await $.get(`/search/bestGenres/${genreId}`)
+    renderer.renderGenresResults(data)
+})
+
+$('.pods').on('click', '.img', function () {
+    let podID = $(this).data('id')
+    console.log(podID)
+    renderer.render2(podManager.getEpisodesData(podID))
+})
+
+//*******render episodes*********//
+
+$('.pods').on('click', '.img-genre-result', function () {
+    let id = $(this).data('id')
+    podManager.getEpisodesData(id)
+    $(window).scrollTop(0)
+})
+
+
+$('.Medium').on('click', function () {
+    renderer.renderGenres(srtGenres.genres)
+    $('.player').empty()
+})
+
+$('.favourites').on('click', function () {
+
+})
+
+$('.bttn').on('click', function () {
+    getPods()
+})
+
+$('.episode-title').on('click', function () {
+})
+
+//********************************************8 */
+
+$('.episodes').on('click', '.episode-title', async function () {
+    let id = $(this).data('id')
+    let result = await $.get(`/episode/${id}`)
+    renderer.renderPlayer(result)
+
+    startplayer();
+    var player;
+
+    function startplayer() {
+        player = document.getElementById('music_player');
+        player.controls = false;
+    }
+    $('#play_button').on('click',function(){
+        player.play()
+    })
+    
+    $('#pause_button').on('click',function(){
+        player.pause()
+    })
+
+    $('#stop_button').on('click',function(){
+        player.pause()
+        player.currentTime=0;
+    })
+
+})
+
+
+Handlebars.registerHelper('each_upto', function (ary, max, options) {
+    if (!ary || ary.length == 0)
+        return options.inverse(this);
+
+    var result = [];
+    for (var i = 0; i < max && i < ary.length; ++i)
+        result.push(options.fn(ary[i]));
+    return result.join('');
+});
+
+$('.podInput').keypress(function (e) {
+    if (e.which == 13) {
+        getPods()
+    }
+});
+
